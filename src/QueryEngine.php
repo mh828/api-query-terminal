0f60e63a9e4f9b9b0a9928ece5def64b33cd19a7
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2025-10-30 18:26:51 +0330

    define return response and generate it in standardize function

--- src/QueryEngine.php
+++ src/QueryEngine.php
@@ -13,10 +13,18 @@ class QueryEngine
 
     private function processor(array $input)
     {
-        foreach ($input as $key => $value) {
+        foreach ($input as $key => $option) {
             if (method_exists($this->entryPoint, $key))
-                $this->responseResult[$key] = call_user_func([$this->entryPoint, $key], ...($value ?? []));
+                $this->responseResult[$key] = $this->responseStandardize(call_user_func([$this->entryPoint, $key], []), ($option['response'] ?? null));
         }
+    }
+
+    private function responseStandardize(mixed $response, ?array $responseOption)
+    {
+        if (is_array($responseOption)) {
+            if (is_array($response)) return array_filter($response, fn($key) => in_array($key, $responseOption), ARRAY_FILTER_USE_KEY);
+        }
+        return $response;
     }
 
     public function response(): mixed

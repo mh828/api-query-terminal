3e6e62335f96972eeea43448cf2adb06501f3f43
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2025-10-30 18:08:46 +0330

    first level processing

--- src/QueryEngine.php
+++ src/QueryEngine.php
@@ -4,8 +4,23 @@ namespace Mh828\ApiQueryTerminal;
 
 class QueryEngine
 {
-    public function __construct(object $entryPoint, array $request)
+    private array $responseResult = [];
+
+    public function __construct(public TypeInterface $entryPoint, public array $request)
     {
-        
+        $this->processor($this->request);
+    }
+
+    private function processor(array $input)
+    {
+        foreach ($input as $key => $value) {
+            if (method_exists($this->entryPoint, $key))
+                $this->responseResult[$key] = call_user_func([$this->entryPoint, $key], ...($value ?? []));
+        }
+    }
+
+    public function response(): mixed
+    {
+        return $this->responseResult;
     }
 }
---
+++ src/TypeInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mh828\ApiQueryTerminal;
+
+interface TypeInterface
+{
+
+}
